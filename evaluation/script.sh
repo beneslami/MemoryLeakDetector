d561527bdcca476f34b7d78912937da9eb4d8bb5
beneslami <beneslami72@gmail.com>
2020-05-22 22:24:55 +0430

    sript to run the program 1000 times

--- evaluation/script.sh
+++ evaluation/script.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 x=1
 while [ $x -le 1000 ]
 do
-	./exe
+	(perf stat -x' ' ./exe_mld) 2>&1 | cut -d' ' -f1 | awk '/^[0-9]/ { printf "%s", $1; }' >> mld_out.txt
+	(perf stat -x' ' ./exe_nrm) 2>&1 | cut -d' ' -f1 | awk '/^[0-9]/ { printf "%s", $1; }' >> nrm_out.txt
+	echo '\n' >> mld_out.txt
+	echo '\n' >> nrm_out.txt 
         x=$(( $x + 1 ))
 done
